e47879c15b4256e044f03801c7004f3dabc94917
Murilo Oliveira <murilonobrega8@gmail.com>
2022-09-27 21:28:20 -0300

    created_aulasPHP

---
+++ aula04/tipos.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="_css/estilo.css"/>
+        <meta charset="UTF-8"/>
+        <title>Curso de PHP - CursoemVideo.com</title>
+    </head>
+
+    <body>
+        <div>
+            <?php
+                //$n = 4;
+                $nome = "Gustavo";
+                $idade = 36;
+                echo $nome." tem ".$idade." anos.<br>";
+                echo "$nome tem $idade anos!";
+            ?>
+        </div>
+    </body>
+</html>
